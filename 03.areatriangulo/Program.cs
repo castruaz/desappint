484e857adbd9f6c14584ba02b74d68b8c4f2253e
Carlos Castaneda Ramirez <castr@uaz.edu.mx>
2019-08-22 12:51:28 -0500

    va

---
+++ 03.areatriangulo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _03.areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float area, b, a;
+
+            Console.WriteLine("Programa que calcula el area de un triangulo");
+
+            if(args.Length<2) {
+                Console.WriteLine("Forma de uso \n");
+                Console.WriteLine("<base> <altura>");
+                Environment.Exit(0);
+            }
+
+            b = float.Parse(args[0]);
+            a = float.Parse(args[1]);
+
+            area =  (b*a) /2 ;
+
+            Console.WriteLine($"El area es {area}");
+
+
+            
+
+
+
+        }
+    }
+}
